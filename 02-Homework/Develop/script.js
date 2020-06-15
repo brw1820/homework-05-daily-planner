76b28bcea49b073b11140d1045034e35a9dead4b
unknown <brw1820@gmail.com>
2020-06-15 09:50:22 -0400

    updated script.js file

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -25,12 +25,12 @@ $(document).ready(function () {
     { twelveHours: "4PM", twentyFourHours: "16" },
     { twelveHours: "5PM", twentyFourHours: "17" },
     { twelveHours: "6PM", twentyFourHours: "18" },
-    { twelveHours: "7PMM", twentyFourHours: "19" },
+    { twelveHours: "7PM", twentyFourHours: "19" },
     { twelveHours: "8PM", twentyFourHours: "20" },
     { twelveHours: "9PM", twentyFourHours: "21" },
     { twelveHours: "10PM", twentyFourHours: "22" },
     { twelveHours: "11PM", twentyFourHours: "23" },
-    { twelveHours: "12PM", twentyFourHours: "24" },
+    { twelveHours: "12AM", twentyFourHours: "24" },
   ];
 
 function newTimeRow() {
@@ -38,6 +38,7 @@ function newTimeRow() {
     var col1 = $("<div>")
       .addClass("col-sm-1 hour")
       .text(hoursObject[hoursIndex].twelveHours);
+      console.log(hoursObject[hoursIndex].twelveHours);
     var col2 = $("<textarea>").addClass("col-sm-10");
     console.log(col2);
     var col3 = $("<button>")
@@ -46,7 +47,7 @@ function newTimeRow() {
     newRow.append(col1).append(col2).append(col3);
     $(".container").append(newRow);
     hoursIndex++;
-    console.log(newTimeRow());
+    // console.log(newTimeRow());
   if (hoursObject[hoursIndex].twentyFourHours === moment().format('H')){
     col2.addClass("present");
   }else if (hoursObject[hoursIndex].twentyFourHours > moment().format('H')){
@@ -56,17 +57,15 @@ function newTimeRow() {
   };
 };
 
+  $("#currentDay").text(moment().format('MMMM Do YYYY'));
   
-  
-  for (var i = 0; i < 9; i++) {
+  for (var i = 0; i < 17; i++) {
     newTimeRow();
-  }
-
-
-  $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+  } 
+  
 });
 
-$("#saveBtn").on("click",)
+
 // classes found in .css folder
 // jumbotron, description, time-block, .row, .hour, .past, .present, .future, .saveBtn, .saveBtn i:hover
 
